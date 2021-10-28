aea809ed8eecc40bc6d560073bc0ac78708a917a
douglas-mir <55262386+douglas-mir@users.noreply.github.com>
2021-10-28 20:52:40 -0300

    FIX VERSION PROBLEM

--- src/main/java/com/example/projectwinter/ProjectWinterApplication.java
+++ src/main/java/com/example/projectwinter/ProjectWinterApplication.java
@@ -2,10 +2,10 @@ package com.example.projectwinter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
 public class ProjectWinterApplication {
 
     public static void main(String[] args) {
